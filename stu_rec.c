c4ca947f02623943b5fcfb60cf7d42131edb3fd0
DesaiNil <nildesai1713@gmail.com>
2023-11-03 20:41:40 +0530

    gb

--- stu_rec.c
+++ stu_rec.c
@@ -9,6 +9,26 @@ typedef struct
      float cgpa;
     char courses[100];
 }Student;
+void addStudent()
+{
+    Student student;
+    FILE *file = fopen("student_records.txt", "a");
+    printf("Add the Students Details\n");
+    printf("-------------------------\n");
+    printf("Enter the first name of student\n");
+    scanf("%s",student.firstName);
+    printf("Enter the last name of student\n");
+    scanf("%s",student.lastName);
+    printf("Enter the Roll number of student\n");
+    scanf("%d",&student.rollNumber);
+    printf("Enter the CGPA of student\n");
+    scanf("%f",&student.cgpa);
+    printf("Enter courses (comma-separated)\n");
+    scanf("%s",student.courses);
+    fprintf(file, "%s %s %d %.2f %s\n", student.firstName, student.lastName, student.rollNumber, student.cgpa, student.courses);
+    fclose(file);
+    printf("Student record added successfully.\n");
+}
 int main()
 {
      int choice;
