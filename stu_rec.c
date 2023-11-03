661746fb244fd7348d047d881763f580bbd17a6d
DesaiNil <nildesai1713@gmail.com>
2023-11-03 20:23:31 +0530

    Update stu_rec.c

--- stu_rec.c
+++ stu_rec.c
@@ -1,9 +1,31 @@
 # include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
 struct Student
 {
-    
+     char firstName[50];
+     char lastName[50];
+     int rollNumber;
+     float cgpa;
+    char courses[100];
 };
-int main(void)
+int main()
 {
-   
+     int choice;
+     while (1)
+      {
+        printf("Student Information Management System\n");
+        printf("1. Add Student\n2. Display Students\n3. Search Student\n4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) 
+        {
+            case 1:
+                addStudent();
+                break;
+        }
+      }
+      return 0;
+
 }
