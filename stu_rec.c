5c87cba23c0ba75c22bcdec6aaa32f2406ddd44e
Sania823 <sani.deshmukh2905@gmail.com>
2023-11-08 09:46:27 +0530

    final

--- stu_rec.c
+++ stu_rec.c
@@ -29,33 +29,7 @@ void saveToFile(struct Student* head)
 void display(struct Student *head)
 
 {
-    //FILE *file;
-    // file = fopen("student_records.txt", "r");
-    // struct Student * temp = head;
-    // while(temp!=NULL){
-        
-    //     printf("Roll Number: %d\n", temp->rollnumber);
-    //     printf("First Name: %s\n", temp->first_name);
-    //     printf("Last Name: %s\n", temp->last_name);
-    //     printf("Branch: %s\n", temp->branch);
-    //     printf("CGPA: %0.4f\n\n", temp->cgpa);
-    //     temp = temp->next;
-    // }
-    // struct Student* current = head;
-    // printf("Student List:\n");
-    // FILE* file = fopen("student_records.txt", "w");
-    // if (file == NULL) {
-    //     printf("Error opening file for writing.\n");
-    //     return;
-    // }
 
-    // while (current != NULL) {
-    //     printf("Roll Number: %d, Name: %s %s, Branch: %s, CGPA: %.2f\n", current->rollnumber, current->first_name, current->last_name, current->branch, current -> cgpa);
-    //     fprintf(file, "Roll Number: %d, Name: %s %s, Branch: %s, CGPA: %.2f\n", current->rollnumber, current->first_name, current->last_name, current->branch, current->cgpa);
-    //     current = current->next;
-    // }
-    // printf("\n");
-    // fclose(file);
     FILE* file = fopen("student_records.txt", "r");
     if (file == NULL) {
         printf("Error opening file for reading.\n");
@@ -115,26 +89,7 @@ void search(struct Student *head,int rollnumber)
 }
 void update(struct Student *head,int rollnumber)
 {
-    // struct Student * temp = head;
-    // while(temp!=NULL)
-    // {
-    // if(temp->rollnumber==rollnumber)
-    //     {
-    //         printf("Record with Roll Number %d Found.\n", rollnumber);
-    //         printf("Enter new First name: ");
-    //         scanf("%s", temp->first_name);
-    //         printf("Enter new Last name: ");
-    //         scanf("%s", temp->last_name);
-    //         printf("Enter new Branch: ");
-    //         scanf("%s", temp->branch);
-    //         printf("Enter new CGPA: ");
-    //         scanf("%f",&temp->cgpa);
-    //         printf("Updation Successful!\n");
-    //         return;
-    //     }
-    //     temp = temp->next;
-    // }
-    // printf("Student with Roll Number %d is not found!\n", rollnumber);
+    
     struct Student* current = head;
 
     while (current != NULL && current->rollnumber != rollnumber) {
@@ -162,28 +117,7 @@ void update(struct Student *head,int rollnumber)
 }
 void delete(struct Student **head,int rollnumber)
 {
-    // struct Student *current = head;
-    // struct Student *prev = NULL;
-    // while (current != NULL)
-    // {
-    //     if (current  -> rollnumber == rollnumber)
-    //     {
-    //         if(prev == NULL)
-    //         {
-    //             current -> next = head;
-    //         }
-    //         else
-    //         {
-    //             prev -> next = current -> next;
-    //         }
-    //         free(current);
-    //         printf("Student with Roll number %d has been deleted\n", rollnumber);
-    //         return;
-    //     }
-    //     prev = current;
-    //     current = current -> next;
-    // }
-    // printf("No Student record corresponding to %d present.\n",rollnumber);struct Student* current = *head;
+    
     struct Student* current = *head;
     struct Student* prev = NULL;
     while (current != NULL && current-> rollnumber != rollnumber) {
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,72 @@
+    //FILE *file;
+    // file = fopen("student_records.txt", "r");
+    // struct Student * temp = head;
+    // while(temp!=NULL){
+        
+    //     printf("Roll Number: %d\n", temp->rollnumber);
+    //     printf("First Name: %s\n", temp->first_name);
+    //     printf("Last Name: %s\n", temp->last_name);
+    //     printf("Branch: %s\n", temp->branch);
+    //     printf("CGPA: %0.4f\n\n", temp->cgpa);
+    //     temp = temp->next;
+    // }
+    // struct Student* current = head;
+    // printf("Student List:\n");
+    // FILE* file = fopen("student_records.txt", "w");
+    // if (file == NULL) {
+    //     printf("Error opening file for writing.\n");
+    //     return;
+    // }
 
-            printf("Enter new CGPA: ");
+    // while (current != NULL) {
+    //     printf("Roll Number: %d, Name: %s %s, Branch: %s, CGPA: %.2f\n", current->rollnumber, current->first_name, current->last_name, current->branch, current -> cgpa);
+    //     fprintf(file, "Roll Number: %d, Name: %s %s, Branch: %s, CGPA: %.2f\n", current->rollnumber, current->first_name, current->last_name, current->branch, current->cgpa);
+    //     current = current->next;
+    // }
+    // printf("\n");
+    // fclose(file);
+    
+
+    // struct Student * temp = head;
+    // while(temp!=NULL)
+    // {
+    // if(temp->rollnumber==rollnumber)
+    //     {
+    //         printf("Record with Roll Number %d Found.\n", rollnumber);
+    //         printf("Enter new First name: ");
+    //         scanf("%s", temp->first_name);
+    //         printf("Enter new Last name: ");
+    //         scanf("%s", temp->last_name);
+    //         printf("Enter new Branch: ");
+    //         scanf("%s", temp->branch);
+    //         printf("Enter new CGPA: ");
+    //         scanf("%f",&temp->cgpa);
+    //         printf("Updation Successful!\n");
+    //         return;
+    //     }
+    //     temp = temp->next;
+    // }
+    // printf("Student with Roll Number %d is not found!\n", rollnumber);
+
+    // struct Student *current = head;
+    // struct Student *prev = NULL;
+    // while (current != NULL)
+    // {
+    //     if (current  -> rollnumber == rollnumber)
+    //     {
+    //         if(prev == NULL)
+    //         {
+    //             current -> next = head;
+    //         }
+    //         else
+    //         {
+    //             prev -> next = current -> next;
+    //         }
+    //         free(current);
+    //         printf("Student with Roll number %d has been deleted\n", rollnumber);
+    //         return;
+    //     }
+    //     prev = current;
+    //     current = current -> next;
+    // }
+    // printf("No Student record corresponding to %d present.\n",rollnumber);struct Student* current = *head;
